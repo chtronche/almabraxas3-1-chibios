4eac8125cda31f723f5589459ddd64d890fea036
Ch. Tronche <ch@tronche.com>
2018-08-05 11:19:53 +0200

    New MPPT implementation to deal with starting (low current) conditions

--- powerManager/powerManager_getPowerBudget.cpp
+++ powerManager/powerManager_getPowerBudget.cpp
@@ -1,22 +1,26 @@
 #include "powerManager.h"
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 volatile int8_t mppt_direction = 2; // start by increasing
 
-power_t power = 0;
+power_t peakPower = 0;
 
 volatile uint16_t powerBudget = 0; // in PWM units
 
 // aka MPPT
 
+#define HYSTERESIS (200) // mW
+
 void powerManager_getPowerBudget(uint8_t voltage, uint8_t current) {
   if (voltage < 75) {
-    powerBudget = power = 0;
+    powerBudget = peakPower = 0;
     mppt_direction = 2;
     return;
   }
   if (!powerBudget) {
     powerBudget = mppt_direction = 2;
-    power = 0;
+    peakPower = 0;
     return;
   }
   // if (current < 0.1Amp) {
@@ -25,11 +29,31 @@ void powerManager_getPowerBudget(uint8_t voltage, uint8_t current) {
   //   return;
   // }
 
-  power_t newPower = voltage * current; // in W / 100
-  // 280 mW hysteresis to start reducing power / immediate power increase 
-  power_t hysteresis = mppt_direction > 0 ? 280 : 0;
-  
-  if (newPower + hysteresis < power) mppt_direction = -mppt_direction;
+  power_t power = voltage * current; // in W / 100
+  if (mppt_direction > 0) {
+    // Climbing
+    uint16_t hysteresis = MAX(50, voltage + voltage / 2);
+    if (power > peakPower - hysteresis) { // That is 1 reading unit of current (100 mA) 
+      if (power > peakPower) peakPower = power;
+      // Go on
+    } else {
+      mppt_direction = -mppt_direction;
+      if (peakPower >= 100) 
+	peakPower -= 100; // mW
+      else
+	peakPower = 0;
+    }
+  } else {
+    // Descending
+    if (power > peakPower) {
+      peakPower = power;
+      mppt_direction = -mppt_direction;
+    } else {
+      if (peakPower >= 100) 
+	peakPower -= 100;
+      else
+	peakPower = 0;
+    }
+  }
   powerBudget += mppt_direction;
-  power = newPower;
 }
--- reporting/reporting_debug.cpp
+++ reporting/reporting_debug.cpp
@@ -3,16 +3,15 @@
 #include "powerManager.h"
 #include "reporting.h"
 
-extern power_t power;
-extern power_t newPower;
+extern power_t peakPower;
 
 static char buffer[256];
 
 extern volatile uint16_t currentReading;
 
 void reporting_debug_loop() {
-  sprintf(buffer, "%ld V=%d I=%d %d P=%d MPPT=%d L=%d R=%d %d\n", getClock(), voltage, current,
+  sprintf(buffer, "%ld V=%d I=%d %d P=%d MPPT=%d L=%d R=%d %d %d\n", getClock(), voltage, current,
 	  currentReading,
-	  powerBudget, mppt_direction, leftPower, rightPower, power);
+	  powerBudget, mppt_direction, leftPower, rightPower, voltage * current, peakPower);
   reporting_debug_print(buffer);
 }
